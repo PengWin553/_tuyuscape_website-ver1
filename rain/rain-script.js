fc5fe4c6864df3a980aaeb37c96cf950f55dea92
PengWin <158315168+PengWin553@users.noreply.github.com>
2024-04-30 23:56:12 +0800

    initial development of tuyuscape

---
+++ rain/rain-script.js
@@ -0,0 +1,12 @@
+function createRaindrops() {
+    const rain = document.querySelector('.rain');
+
+    for (let i = 0; i < 100; i++) {
+        const raindrop = document.createElement('div');
+        raindrop.classList.add('raindrop');
+        raindrop.style.left = `${Math.random() * 100}%`;
+        raindrop.style.animationDelay = `${Math.random()}s`;
+        rain.appendChild(raindrop);
+    }
+}
+createRaindrops();
